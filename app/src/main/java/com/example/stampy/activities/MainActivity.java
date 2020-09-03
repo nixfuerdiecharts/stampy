f605b789b0ce3cc4950158b4a8a246a58e02d52d
nixfuerdiecharts <peldaftw@gmail.com>
2020-09-03 13:20:01 +0200

    Added Edit clock in time button

--- app/src/main/java/com/example/stampy/activities/MainActivity.java
+++ app/src/main/java/com/example/stampy/activities/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.stampy.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.preference.PreferenceManager;
 
+import android.app.ActivityManager;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -13,7 +18,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.stampy.R;
 import com.example.stampy.broadcast.StampTimerBroadcastListener;
@@ -22,18 +29,23 @@ import com.example.stampy.broadcast.StampTimerBroadcastReceiver;
 import org.w3c.dom.Text;
 
 import java.text.DecimalFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, StampTimerBroadcastListener {
 
     private SharedPreferences sp;
-    private Button btn_stamp;
+    private AppCompatButton btn_stamp;
+    private AppCompatImageButton btn_edit_clock_in;
 
     private StampTimerBroadcastReceiver broadcastReceiver;
+
+    private boolean stampedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         init();
+        stampedIn = false;
     }
 
     @Override
@@ -71,6 +83,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Button OnCLickListener
         btn_stamp = findViewById(R.id.btn_stamp);
         btn_stamp.setOnClickListener(this);
+        btn_edit_clock_in = findViewById(R.id.btn_edit_clock_in);
+        btn_edit_clock_in.setOnClickListener(this);
 
         //Init timer
         TextView tv_time_min = findViewById(R.id.tv_time_min);
@@ -108,8 +122,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_stamp:
                 stampedPressed();
                 break;
+            case R.id.btn_edit_clock_in:
+                editStampTimePressed();
             default://TODO: unhandled
         }
+    }
+
+    private void editStampTimePressed() {
+        Calendar mcurrentTime = Calendar.getInstance();
+        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = mcurrentTime.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                //TODO: Handle
+            }
+        }, hour, minute, true);//Yes 24 hour time
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();
     }
 
     private void stampedPressed() {
@@ -117,6 +148,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Intent intent = new Intent(this, EggTimerService.class);
         //intent.putExtra(EggTimerService.EGG_ORDER_EXTRA_KEY, order);
         //startService(intent);
+
+        // TODO: Make Following situational, only execute if currently clocked out
+        // isMyServiceRunning(ServiceName.class) in IF-Statement
+        if(stampedIn) {
+            stampedIn = !stampedIn;
+            // Swap stamp Button
+            btn_stamp.setText(R.string.btn_clock_in);
+            // Hide Edit Button
+            btn_edit_clock_in.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    0
+            ));
+            btn_edit_clock_in.setVisibility(View.GONE);
+
+        } else {
+            stampedIn = !stampedIn;
+            // Swap stamp Button
+            btn_stamp.setText(R.string.btn_clock_out);
+            // Show Edit Button
+            btn_edit_clock_in.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    1
+            ));
+            btn_edit_clock_in.setVisibility(View.VISIBLE);
+        }
+
+
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void updateTimerValue(int remainingSeconds, TextView tv) {
